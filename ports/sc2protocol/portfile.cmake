fe9288d0788e13d638a47ebf37c03ab6f232b64b
Joshua "Shadow" Monson <4204089+ShadowLordAlpha@users.noreply.github.com>
2024-12-15 12:50:33 -0700

    update copy

--- ports/sc2protocol/portfile.cmake
+++ ports/sc2protocol/portfile.cmake
@@ -10,4 +10,4 @@ file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 vcpkg_cmake_install()
-vcpkg_install_copyright("${SOURCE_PATH}/PROTOCOL_LICENSE")
+vcpkg_install_copyright(FILE_LIST  "${SOURCE_PATH}/PROTOCOL_LICENSE")
